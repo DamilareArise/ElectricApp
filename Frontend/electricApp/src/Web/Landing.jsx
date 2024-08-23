bb70431724b9cb145a582ee7ab977d5c9b20fbf0
Arise Damilare <arisedamilare5@gmail.com>
2024-08-23 19:50:10 +0100

    refactor changed for deployment sake

--- Frontend/electricApp/src/Web/Landing.jsx
+++ Frontend/electricApp/src/Web/Landing.jsx
@@ -1,25 +1,25 @@
-// import React from 'react'
+import React from 'react'
 
-// import Footer from "../components/Footer"
-// import Header from "../components/Header"
-// import Nav from "../components/nav"
-// import Section1 from "../components/section1"
-// import Section2 from "../components/Section2"
-// import Section3 from "../components/Section3"
-// import Section4 from "../components/Section4"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import Nav from "../components/nav"
+import Section1 from "../components/section1"
+import Section2 from "../components/Section2"
+import Section3 from "../components/Section3"
+import Section4 from "../components/Section4"
 
-// const Landing = () => {
-//   return (
-//     <div>
-//         <Nav/>
-//         <Header/>
-//         <Section1/>
-//         <Section2/>
-//         <Section3/>
-//         <Section4/>
-//         <Footer/>
-//     </div>
-//   )
-// }
+const Landing = () => {
+  return (
+    <div>
+        <Nav/>
+        <Header/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Footer/>
+    </div>
+  )
+}
 
-// export default Landing
+export default Landing
