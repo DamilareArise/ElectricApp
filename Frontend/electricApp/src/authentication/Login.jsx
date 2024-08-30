0254c54ebea0141513f87061cfd78283984200d7
Onifade Joshua <onifadjosh@gmail.com>
2024-08-30 14:39:17 +0100

    Updated Login and SignUp components with minor styling changes and added loading state to Login button

--- Frontend/electricApp/src/authentication/Login.jsx
+++ Frontend/electricApp/src/authentication/Login.jsx
@@ -1,4 +1,4 @@
-import  { useState} from 'react';
+import {useState } from 'react';
 import loginImg from "../assets/loginImg.jpg";
 import message from "../assets/message.svg";
 import passwordd from "../assets/password.svg";
@@ -10,7 +10,9 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-  let navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
+
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
@@ -71,6 +73,8 @@ const Login = () => {
       return;
     }
 
+    setLoading(true);
+
     try {
       const response = await fetch(`https://electricapp.onrender.com/account/signin`, {
         method: 'POST',
@@ -83,45 +87,40 @@ const Login = () => {
         }),
       });
 
-      if (response.ok) {
-        const data = await response.json();
-        
-        if (data.status){
-          console.log('Login successful:', data);
-          //na this place you route go another page
-          localStorage.token = data.token
-          navigate('/dashboard')
-        }
-        else{
-          console.log('Login failed:', data);
-          }
-        setPasswordError('Login failed. Please check your credentials.');
-        
+      const data = await response.json();
+
+      if (response.ok && data.status) {
+        console.log('Login successful:', data);
+        localStorage.token = data.token;
+        navigate('/dashboard');
       } else {
+        console.log('Login failed:', data);
         setPasswordError('Login failed. Please check your credentials.');
       }
     } catch (error) {
       console.error('Error:', error);
       setPasswordError('An error occurred. Please try again later.');
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="bg-[url('./assets/loginImg.jpg')]  lg:bg-none bg-cover bg-center h-[100vh] lg:h-fit">
-      <Navbar/>
+    <div className="bg-[url('./assets/loginImg.jpg')] lg:bg-none bg-cover bg-center h-[100vh] lg:h-fit">
+      <Navbar />
 
-      <div className="flex /bg-[url('./assets/loginImg.jpg')] items-center justify-center lg:justify-between h-full lg:h-fit gap-[104px] px-[24px] lg:pl-[57px] lg:pr-[57px] mt-[50px] mb-[58px] pt-[70px]">
-        <img src={loginImg} alt="Login" width={323} height={323} className="w-[40%] loginimg hidden lg:block" />
-        <div className="md:w-[70%] lg:w-[50%] pt-[32px] pb-[32px] bg-[#FFFEFE] shadow-md shadow-[#00000040] px-[26px] rounded-[30px] ">
+      <div className="flex items-center justify-center lg:justify-between h-full lg:h-fit gap-[104px] px-[24px] lg:pl-[57px] lg:pr-[57px] mt-[50px] mb-[58px] pt-[70px]">
+        <img src={loginImg} alt="Login" width={323} height={323} className="w-[40%] hidden lg:block" />
+        <div className="md:w-[70%] lg:w-[50%] pt-[32px] pb-[32px] bg-[#FFFEFE] shadow-md shadow-[#00000040] px-[26px] rounded-[30px]">
           <form onSubmit={handleSubmit}>
-            <p className="pb-[32px] text-center">Login</p>
+            <p className="pb-[32px] text-center text-[16px] md:text-[28px] font-[500]">Login</p>
 
             <div className="flex items-center bg-[#F5F5F5] gap-[17.43px] rounded-[11.62px] px-[25.56px] shadow-md shadow-[#00000040] mb-[10px]">
               <img src={message} alt="Email" width={22.07} height={17.43} />
               <input
                 type="email"
                 placeholder="Email"
-                className="py-[20.91px] bg-[#F5F5F5] border-none outline-none w-full"
+                className="py-[20.91px] bg-[#F5F5F5] border-none outline-none w-full placeholder:text-[23.3px]"
                 value={email}
                 onChange={handleEmailChange}
               />
@@ -133,7 +132,7 @@ const Login = () => {
               <input
                 type="password"
                 placeholder="Password"
-                className="py-[20.91px] bg-[#F5F5F5] border-none outline-none w-full"
+                className="py-[20.91px] bg-[#F5F5F5] border-none outline-none w-full placeholder:text-[23.3px]"
                 value={password}
                 onChange={handlePasswordChange}
               />
@@ -142,18 +141,19 @@ const Login = () => {
 
             <button
               type="submit"
-              className="py-[22px] bg-[#EDA145] rounded-tl-[20px] rounded-br-[20px] w-full mb-[22px]"
+              className={`py-[22px] bg-[#EDA145] rounded-tl-[20px] rounded-br-[20px] w-full mb-[22px] md:text-[20px] font-[400] text-[16px]  hover:opacity-[75%]`}
+              disabled={loading}
             >
-              Login
+              {loading ? 'Loading...' : 'Login'}
             </button>
 
             <a href="#" className="w-full">
-              <p className="text-right">Forget password?</p>
+              <p className="text-right md:text-[20px] font-[400] text-[16px]">Forget password?</p>
             </a>
 
             <div className="text-center mt-[32px]">
-              <p>
-                Already a registered member? <Link to='/SignUp'>Sign Up</Link>
+              <p className="md:text-[20px] font-[400] text-[16px]">
+                Already a registered member? <Link to='/SignUp' className='font-[400] text-[16px] md:text-[20px]'>Sign Up</Link>
               </p>
             </div>
           </form>
